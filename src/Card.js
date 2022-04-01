e97ef0dc93eeca0b69e5776176b16cb391d41aff
ALI_MALIK <ali.haider@carbonteq.com>
2022-04-01 21:32:03 +0500

    react pagination & intersection observer api

--- src/Card.js
+++ src/Card.js
@@ -1,28 +1,23 @@
 import React from "react";
-import { Card, Avatar, Row, Col } from "antd";
+import { Card, Avatar, Row, Col, Typography } from "antd";
 const { Meta } = Card;
+const { Title } = Typography;
 
 const Cards = (props) => {
   console.log("props data is ....", props);
   console.log("email data is ....", props.data.email);
 
   return (
-    <div className="card-data">
-      {/* <Row>
-        <Col span={8}> */}
+    <div style={{ marginTop: "20px" }}>
       <Card
         style={{ width: 300 }}
         cover={<img alt="example" src={props.data.picture.large} />}
       >
-        <li>
-          <h4>{props.data.name.first}</h4>
-          <h4>{props.data.name.last}</h4>
-          <h4>{props.data.login.username}</h4>
-          <h4>{props.data.email}</h4>
-        </li>
+        <Title level={5}>{props.data.name.first}</Title>
+        <Title level={5}>{props.data.name.last}</Title>
+        <Title level={5}>{props.data.login.username}</Title>
+        <Title level={5}>{props.data.email}</Title>
       </Card>
-      {/* </Col>
-      </Row> */}
     </div>
   );
 };
--- src/users.js
+++ src/users.js
@@ -1,33 +1,112 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import Cards from "./Card";
-import { Card, Avatar, Row, Col } from "antd";
+import { Card, Avatar, Row, Col, Typography } from "antd";
+import { Button, Radio } from "antd";
+const { Title } = Typography;
 
 const Users = () => {
-  //   const url = "https://randomuser.me/api";
   const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(0);
+  const [isVisible, setIsVisible] = useState(false);
+  // const [check, setCheck] = useState(false);
+  const myRef = useRef();
+
+  // const options = {
+  //   threshold: 1.0,
+  // };
+  useEffect(() => {
+    if (page <= 5) {
+      const observer = new IntersectionObserver((entries) => {
+        const entry = entries[0];
+        console.log("entry", entry);
+        if (entry && entry.isIntersecting) {
+          console.log(
+            "entry  is ...........................",
+            entry.isIntersecting
+          );
+          setIsVisible(entry.isIntersecting);
+          console.log("visibility is ......", isVisible);
+
+          // }
+        }
+      });
+      if (myRef.current) {
+        observer.observe(myRef.current);
+      }
+      return () => {
+        if (myRef.current) {
+          observer.unobserve(myRef.current);
+        }
+      };
+    }
+  }, [myRef]);
 
   useEffect(() => {
-    axios
-      .get("https://randomuser.me/api/?results=10")
-      .then((response) => {
-        setUsers(response.data.results);
-        console.log(response);
-      })
-      .catch((err) => console.log(err));
-  }, []);
+    console.log("page is ....", page);
+    console.log("myref", myRef.current);
+    axios({
+      method: "GET",
+      url: `https://randomuser.me/api/?page=${page}&results=10`,
+    }).then((response) => {
+      console.log("user data before ...", users);
+      console.log("new data .....", response.data.results);
+      const data = response.data.results;
+
+      setUsers([...users, ...response.data.results]);
+
+      // console.log("asdjaksda", isVisible);
+      if (isVisible) {
+        setPage(page + 1);
+        setIsVisible(false);
+        console.log("visible again set to ......", isVisible);
+      }
+    });
+  }, [page, isVisible, myRef]);
+
+  // useEffect(() => {
+  //   axios
+  //     .get("https://randomuser.me/api/?results=10")
+  //     .then((response) => {
+  //       setUsers(response.data.results);
+  //       console.log(response);
+  //     })
+  //     .catch((err) => console.log(err));
+  // }, []);
 
   return (
     <>
-      <h1 style={{ textAlign: "center", marginTop: "20px" }}>List of Users </h1>
-
+      <Title level={1} className="heading">
+        List of users{" "}
+      </Title>
       {console.log("user data ...", users)}
-      <div>
-        <div className="user-data">
-          {users.map((item, index) => (
+
+      <Row
+        style={{
+          padding: "100px",
+          marginBottom: "300px",
+        }}
+      >
+        {users.map((item, index) => (
+          <Col lg={{ span: 6 }} xs={{ span: 24 }}>
             <Cards data={users[index]} key={index} />
-          ))}
-        </div>
+          </Col>
+        ))}
+      </Row>
+      <div
+        className="btn-btn"
+        ref={myRef}
+        style={{ backgroundColor: "red", height: "20vh" }}
+      >
+        {/* <Button
+          type="primary"
+          size={10}
+          onClick={() => {
+            setPage(page + 1);
+          }}
+        >
+          Load More
+        </Button> */}
       </div>
     </>
   );
