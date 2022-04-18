63262734461ceef1e635ceb6912ed1e95585c6d8
ALI_MALIK <ali.haider@carbonteq.com>
2022-04-18 17:28:00 +0500

    JSX issues resolved

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import User from "../Components/users/Users";
-import Selector from "../Redux/selectors/selector";
+import User from "../../Components/users/Users";
+import Selector from "../../Redux/selectors/selector";
 const user = () => {
   const { error, filter, searchData, pageNo } = useSelector(
     (state) => state.app
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,22 +3,15 @@ import Search from "../Components/search/Search";
 import Users from "../Components/users/Users";
 import Layout from "../Components/layout/Layoutwrap.js";
 import { useSelector, useDispatch } from "react-redux";
-import UserContainer from "../container/userContainer";
+import UserContainer from "../Components/home/Home.jsx";
 
 const Home = () => {
   const { user } = useSelector((state) => state.app);
-
-  // const data = {
-  //   user,
-  //   filter,
-  //   searchData,
-  // };
 
   return (
     <Layout>
       <Search data={user} />
       <UserContainer />
-      {/* <Users /> */}
     </Layout>
   );
 };
