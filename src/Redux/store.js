ae79b5ea5f26eade0f7a78033b13bfdfda9195f4
ALI_MALIK <ali.haider@carbonteq.com>
2022-04-25 15:16:14 +0500

    Fixes

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "../../src/Redux/reducers/userReducer";
-import settingReducer from "../../src/Redux/reducers/settingsReducer";
+import userReducer from "./reducers/userReducer";
+import settingReducer from "./reducers/settingsReducer";
 export const store = configureStore({
   reducer: {
     app: userReducer,
